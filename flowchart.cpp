39b47b872cc5686361e5a15f194045e03748837c
Jane <ngaukonojane@gmail.com>
2018-02-19 16:25:33 +0300

    fifth commit

---
+++ flowchart.cpp
@@ -0,0 +1,35 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    int b;
+    
+    cout<<"enter the first number";
+    cin>>a;
+    cout<<"enter the second number";
+    cin>>b;
+    
+    if (a>b)
+    {
+        cout<<"first number is greater than second number";
+    }
+    else if(a==b)
+    {
+        cout<<"secod number is greater than first number";
+        
+    }
+    
+
+    return 0;
+}
+
+
